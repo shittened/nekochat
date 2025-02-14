d58aeb1a9be7cc67d19db5f6eab5f06113f2e290
shitten <kiefciman@gmail.com>
2025-02-14 18:42:16 +0200

    a

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var email_exists = document.cookie.indexOf('email=')
 var password_exists = document.cookie.indexOf('password=')
 
+alert('if any1 uses firefox, is broken af in firefox u cant even get to auhentification page, dont use firefox w this for now')
+
 if (email_exists != -1 && password_exists != -1) {
     document.querySelector('#page').src = 'chat.html'
 }
